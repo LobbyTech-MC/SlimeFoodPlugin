016407115eb1d11d011a11d8e42217f1bb416338
LobbyTech <1187755698@qq.com>
2025-11-16 11:29:22 +0800

    1

--- src/main/java/me/gamingoninsulin/slimefood/SlimeFoodPlugin.java
+++ src/main/java/me/gamingoninsulin/slimefood/SlimeFoodPlugin.java
@@ -42,7 +42,7 @@ public class SlimeFoodPlugin extends JavaPlugin implements SlimefunAddon {
         ItemStack[] multiblock = SFBrickOven.getMultiBlockPattern(); // Use the method from SFBrickOven class
         ItemStack[] recipe = new ItemStack[]{new ItemStack(Material.APPLE), new ItemStack(Material.SUGAR), null, null, null, null, null, null, null};
         sfBrickOven = new SFBrickOven(itemGroup, sfBrickOvenItem, multiblock, recipe, BlockFace.SELF);
-        itemManager.registerSFBrickOven(sfBrickOven);
+        //itemManager.registerSFBrickOven(sfBrickOven);
         itemGroupManager.addItem(sfBrickOven);
 
         getLogger().info("SlimeFood Multibiocks has been enabled!");
